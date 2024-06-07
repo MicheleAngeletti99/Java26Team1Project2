9aa3cff03764b9668a11b06e8e2a87536f4da408
Michele Angeletti <m.angeletti99@gmail.com>
2024-06-07 15:17:31 +0200

    aggiunta classe Bevande

---
+++ src/Bevande.java
@@ -0,0 +1,6 @@
+public class Bevande extends Portata{
+
+    public Bevande(String nome, Double prezzo, String descrizione, String ingredienti) {
+        super(nome, prezzo, descrizione, ingredienti);
+    }
+}

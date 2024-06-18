8e66682579240843bdb0b38146016778c3ad6440
iresha warnakulasuriya <dilhanimadu2002@gmail.com>
2024-06-18 22:19:39 +0200

    aggiornato il classe Desserts

--- src/Desserts.java
+++ src/Desserts.java
@@ -1,10 +1,30 @@
 public class Desserts extends Portata {
 
+    // i field
+    private Boolean glutenFree;
+
     // override dei costruttore
-    public Desserts(String nome, Double prezzo, String descrizione, String ingredienti) {
+    public Desserts(String nome, Double prezzo, String descrizione, String ingredienti, Boolean glutenFree) {
         super(nome, prezzo, descrizione, ingredienti);
+        this.glutenFree = glutenFree;
+    }
+
+    // i getter and setter
+
+    public Boolean getGlutenFree() {
+        return glutenFree;
+    }
+
+    public void setGlutenFree(Boolean glutenFree) {
+        this.glutenFree = glutenFree;
     }
 
 
+    // stampadettagli method from parent class
 
+    @Override
+    public void stampaDettagli() {
+        super.stampaDettagli();
+        System.out.println("is this Vegan: " + glutenFree);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,11 @@ public class Main {
         primi3.stampaDettagli();
 
         // desserts
-        Desserts tiramisu = new Desserts("Tiramisu", 3.99,"dessert, di origine italiana", "ladyfingers (savoiardi), egg yolks, sugar, coffee, mascarpone and cocoa powder.");
+        Desserts tiramisu = new Desserts("Tiramisu", 3.99,"dessert, di origine italiana", "ladyfingers (savoiardi), egg yolks, sugar, coffee, mascarpone and cocoa powder.",false);
         tiramisu.stampaDettagli();
-        Desserts cheeseCake = new Desserts("cheeseCake",4.00,"un dolce a base di formaggio fresco a pasta molle"," eggs, and sugar. It may have a crust or base made from crushed cookies (or digestive biscuits), graham crackers, pastry, or sometimes sponge cake.");
+        Desserts cheeseCake = new Desserts("cheeseCake",4.00,"un dolce a base di formaggio fresco a pasta molle"," eggs, and sugar. It may have a crust or base made from crushed cookies (or digestive biscuits), graham crackers, pastry, or sometimes sponge cake.", true);
         cheeseCake.stampaDettagli();
-        Desserts chocolateFudge = new Desserts("Chocolate Fudge", 5.00,"a dense, rich confection", "sugar, milk or cream, butter and chocolate or other flavorings.");
+        Desserts chocolateFudge = new Desserts("Chocolate Fudge", 5.00,"a dense, rich confection", "sugar, milk or cream, butter and chocolate or other flavorings.", false);
         chocolateFudge.stampaDettagli();
 
         // Bevande

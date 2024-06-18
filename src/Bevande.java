bb33873b099df2f4703d1f3987ffedcaebec7ead
Michele Angeletti <m.angeletti99@gmail.com>
2024-06-18 09:54:53 +0200

    aggiornata la classe bevande

--- src/Bevande.java
+++ src/Bevande.java
@@ -1,6 +1,36 @@
-public class Bevande extends Portata{
+public class Bevande extends Portata {
 
-    public Bevande(String nome, Double prezzo, String descrizione, String ingredienti) {
+    private Integer annoDiProduzione;
+    private Boolean certificazioneDOP;
+
+    public Bevande(String nome, Double prezzo, String descrizione, String ingredienti, Integer annoDiProduzione, Boolean certificazioneDOP) {
         super(nome, prezzo, descrizione, ingredienti);
+        this.annoDiProduzione = annoDiProduzione;
+        this.certificazioneDOP = certificazioneDOP;
+    }
+
+    public Integer getAnnoDiProduzione() {
+        return annoDiProduzione;
+    }
+
+    public void setAnnoDiProduzione(Integer annoDiProduzione) {
+        this.annoDiProduzione = annoDiProduzione;
+    }
+
+    public Boolean getCertificazioneDOP() {
+        return certificazioneDOP;
+    }
+
+    public void setCertificazioneDOP(Boolean certificazioneDOP) {
+        this.certificazioneDOP = certificazioneDOP;
+    }
+
+    @Override
+    public void stampaDettagli() {
+        super.stampaDettagli();
+        System.out.println("Prodotto nell'anno " + annoDiProduzione);
+        if (certificazioneDOP) {
+            System.out.println("Prodotto con certificazione DOP");
+        }
     }
 }

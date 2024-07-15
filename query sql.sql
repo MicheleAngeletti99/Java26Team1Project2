53bac2ad1679d4160f7e024e84fa1b27e196baaf
Riccardo <ric.cavaleri@gmail.com>
2024-07-15 18:31:30 +0200

    Aggiornato query con tab.antipasti e tab.menu

--- query sql.sql
+++ query sql.sql
@@ -1,4 +1,17 @@
+#creazione del database
 create database progetto_ristorante;
+#selezione del database su cui si lavora
+use progetto_ristorante;
+
+
+
+ #creo tabella menu
+ create table Menu(
+ Id int primary key auto_increment,
+ tipo_menu enum('CARNE','PESCE','VEGETARIANO','VEGANO','DELGIORNO','FINESETTIMANA','ORDINARIO','COMPLETO')
+ );
+ insert INTO Menu (tipo_menu)
+ values('CARNE');
 
 # Creazione tabella bevande
 create table bevande (
@@ -48,3 +61,45 @@ values ('Alberto','Pippa'),
        ('Riccardo','Cavaleri'),
        ('Andrea','Vecchione'),
        ('Santo','La Rocca');
+  
+  #creazione tabella antipasti
+  create table Antipasti(
+  Id int primary key auto_increment,
+  nome varchar (255),
+  prezzo decimal (10,2),
+  descrizione varchar(255),
+  ingredienti varchar(255),
+  origine_prodotto varchar(255)
+  );
+ 
+#creazione colonna per riferimento chiave esterna
+alter table Antipasti 
+add column menu_id int;
+
+#chiave esterna
+alter table Antipasti
+add foreign key (menu_id) references menu(Id);
+
+#inserisco valori all'interno di antipasti
+  insert into Antipasti (nome, prezzo, descrizione, ingredienti, origine_prodotto)
+  values('Bruschette',5.99,'pane tostato e pomodoro', 'pane, pomodoro, olio, aglio', 'Pomodori e aglio originari dalla Sicilia, pane di nostra produzione'),
+  		('Misto caldo',8.99,'patatine fritte, arancine fritte e panelle fritte','patate, farina di ceci, palle di riso fritte','Prodotti originari dal tipo streetFood Siciliano'),
+  		('Polenta fritta',10.55,'polenta fritta servita con funghi','farina di mais e funghi','Prodotti e tipo di piatto originario dalla Toscana');
+  	
+  #aggiungo una portata alla tabella		
+  insert INTO Antipasti (nome, prezzo, descrizione, ingredienti, origine_prodotto)
+  values('Patatine Fritte',3.50,'patate lavate e fritte','patate','Patate originarie del Piemonte');
+ 
+#aggiorno il valore di menu id in base al tipo di menu scelto (1)
+ update antipasti
+ SET menu_id = 1
+ where id IN (1,2,3,4);
+ 
+#creazione tabella per relazione N a N
+create table Menu_Ristorante (
+menu_id int not null,
+ristorante_id int not null,
+primary key (menu_id, ristorante_id),
+foreign key (menu_id) references menu(id),
+foreign key (ristorante_id) references ristoranti(id)
+);
